f94a3758fa8ffb9776b99644add4b951b4a8d5ec
Melany Gulianovych <melany.gulianovych@ndeema.com>
2016-06-16 00:04:38 +0300

    Initial Commit

---
+++ SizeClassesApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SizeClassesApp
+//
+//  Created by Melany Gulianovych on 6/16/16.
+//  Copyright © 2016 Melany Gulianovych. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SizeClassesApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SizeClassesApp
+//
+//  Created by Melany Gulianovych on 6/16/16.
+//  Copyright © 2016 Melany Gulianovych. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
